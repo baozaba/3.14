9ad6d2528979e7564efad949f3c6b00cf154e2a8
= <=>
2024-03-14 19:34:39 +0800

    BalancedParentheses

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        System.out.print("Please enter an expression using only parentheses: ");
+        Scanner reader = new Scanner(System.in);
+        String expression = reader.nextLine();
+        BalancedParentheses balance = new BalancedParentheses();
+        balance.balanceParentheses(expression);
+        reader.close();
+    }
+}
